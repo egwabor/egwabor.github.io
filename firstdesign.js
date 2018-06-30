8a29a66fb3128fc17903792d8a12d00b05248338
egwabor <40705089+egwabor@users.noreply.github.com>
2018-06-29 19:08:49 -0700

    Add files via upload

---
+++ firstdesign.js
@@ -0,0 +1,35 @@
+// Select color input
+// Select size input
+const height, width, color;
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event) {
+    event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWeight').val();
+	
+	makeGrid(height, width);
+	
+	//console.log(height);
+});
+function makeGrid(a, b) {
+    $('tr').remove();
+
+// Your code goes here!
+    for(let i = 1; i <= a; i++){
+	    $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+		for(let j = 1; j <= b; j++){
+		    $('#table' + i).append('<td></td>');
+		}
+	}
+	
+	//add color to the cell
+	$('td').click(function addColor(){
+	    color = $('#colorPicker').val();
+		
+		if($(this).attr('style')) {
+		    $(this).removeAttr('style');
+		} else {
+		    $(this).attr('style', 'background-color: ' + color);
+		}
+	});
+}
---
+++ seconddesign.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#sizePicker').submit(function makeGrid(grid){
+	    $('table tr').remove();
+		const rows = $('#inputHeight').val();
+		const cols = $('#inputWeight').val();
+		
+		for(let i = 1; i <= rows; i++) {
+		    $('#pixelCanvas').append('<tr id="class"'+ i + '></tr>');
+			
+			for(let j = 1; j <= cols; j++) {
+		        $('tr:last').append('<td></td>');
+				$('td').attr('class', 'cells');
+		    }
+		}
+		grid.preventDefault();
+		
+		$('.cells').click(function(event) {
+		    var paint = $('#colorPicker').val();
+			if($(this).attr('style')){
+			    $(this).removeAttr('style');
+			} else {
+			    //$(event.target).css('background-color', paint);
+				$(event.target).attr('style', 'background-color: ' + paint);
+			}
+			
+		});
+	});
+});
